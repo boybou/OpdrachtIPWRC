78cec0923fe48c06b7fcc568126f14fed09b478d
boybou <boy.bouwense@gmail.com>
2017-12-17 16:00:48 +0100

    Initial commit

---
+++ src/main/java/API/ApiGuiceModule.java
@@ -0,0 +1,15 @@
+package API;
+
+import com.google.inject.AbstractModule;
+
+/**
+ * Created by boybo on 17-12-2017.
+ */
+public class ApiGuiceModule extends AbstractModule {
+    @Override
+    protected void configure()
+    {
+
+    }
+
+}
